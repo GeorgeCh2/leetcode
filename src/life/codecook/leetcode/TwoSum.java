c2a869b5dcb5ec57415ac75221ca74e1aa0fc0f2
zhouzhi <zhouzhi@qbb6.com>
2020-03-15 12:12:39 +0800

    init and add TwoSum

---
+++ src/life/codecook/leetcode/TwoSum.java
@@ -0,0 +1,27 @@
+package life.codecook.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和
+ *
+ * @author georgechmr@gmail.com
+ * @date 2020-03-15 11:53
+ */
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> hashTable = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (hashTable.containsKey(complement)) {
+				return new int[]{i, hashTable.get(complement)};
+			}
+
+			hashTable.put(nums[i], i);
+		}
+
+		throw new IllegalArgumentException("No two num solution");
+	}
+}
